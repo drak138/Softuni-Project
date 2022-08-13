e070697b6d231a4411c734edcf80cee4fa8d2116
drak138 <100386199+drak138@users.noreply.github.com>
2022-08-13 19:10:15 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import { Header} from './components/header/Header';
 import Menu from'./components/menu/Menu'
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import { auth } from './firebase';
+import { All } from './components/menu/all';
+import {onAuthStateChanged,signOut} from 'firebase/auth'
+import {Route,Routes} from 'react-router-dom'
+import{BitcoinPrice,EtheriumPrice,BNBPrice} from './components/menu/menu function/Crypto-values/CryptoPrices'
 
-function App() {
+function App(props) {
+  
+const [ShowMessage,setShowMessage]=useState(false)
+    
   return (
     <div className="App">
       <Header/>
-      <div className='line'/>
-     <Menu/>
+    <div className='line'/>
+    <Menu/>
+      <Routes>
+              <Route path="/etherium" element={<EtheriumPrice/>}></Route>
+              <Route path="/BNB" element={<BNBPrice/>}></Route>
+              <Route path="/BTC" element={<BitcoinPrice/>}></Route>
+              <Route path="/All" element={<All/>}></Route>
+      </Routes>    
+
     </div>
   );
 
