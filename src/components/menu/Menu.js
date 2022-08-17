8078be4d9676850bb52dceb3c1450ecd642d74bb
drak138 <100386199+drak138@users.noreply.github.com>
2022-08-17 14:56:07 +0300

    Update Menu.js

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -2,8 +2,7 @@ import styles from './Menu.module.css'
 import React, { useState, useEffect } from 'react';
 import { auth } from '../../firebase';
 import {onAuthStateChanged,signOut} from 'firebase/auth'
-import {BrowserRouter as Router, Link, Route ,Routes} from 'react-router-dom'
-import{BitcoinPrice,EtheriumPrice,BNBPrice} from './menu function/Crypto-values/CryptoPrices'
+import {Link} from 'react-router-dom'
 function Menu(props){
   const [showcrypto,setShowcrypto]=useState(false);
 
@@ -17,12 +16,6 @@ function Menu(props){
    signOut(auth);
   }, [])
 
-    const handleClick=()=>{
-        setisActive(current => !current);
-        setisActiveBit(true);
-        setisActiveBNB(true);
-        setisActiveEth(true)
-    }
    
     return(
       <div className='wrapper'>  
@@ -36,29 +29,29 @@ function Menu(props){
           <div className={styles.line2}/>
           <li  style={{gap:'10px'}}>
             
-            <p onClick={()=>setisActive(false)}> <Link style={{color:'black', textDecoration:'none'}} to='/etherium'>Etherium</Link></p>
+            <p> <Link style={{color:'black', textDecoration:'none'}} to='/etherium'>Etherium</Link></p>
             
           </li>          
           <div className={styles.line2}/>
           <li style={{gap:'77px'}}>
             
-            <p onClick={()=>setisActive(false)}><Link style={{color:'black', textDecoration:'none'}} to='/BNB'>BNB</Link></p>
+            <p ><Link style={{color:'black', textDecoration:'none'}} to='/BNB'>BNB</Link></p>
   
           </li>
           <div className={styles.line2}/>
           {User?<li>
             
-            <p onClick={()=>setisActive(false)}> <Link style={{color:'black', textDecoration:'none'}} to='/BTC'>Bitcoin</Link></p>
+            <p> <Link style={{color:'black', textDecoration:'none'}} to='/BTC'>Bitcoin</Link></p>
           
             </li>:<p style={{fontSize:'21px', fontWeight:'400'}}>you have to be logged in</p>}
         </ul>:null
         }
       </button>
       </div>
-      {User?<button >
-        <p><Link style={{color:'black', textDecoration:'none'}} to='/All'>All</Link>
+      {User?
+        <p style={{margin:'0'}}><Link className={styles.All} to='/All'>All</Link>
         </p>
-      </button>:<button onClick={()=>setShowMessage(current=>!current)}><p>all</p></button>
+        :<button onClick={()=>setShowMessage(current=>!current)}><p>all</p></button>
       }
       {ShowMessage?<div onClick={()=> setShowMessage(false)} style={{backgroundColor: '#46507c',border: '2px solid black',borderRadius: '20px',position: 'absolute',zIndex: '1',display:'flex',flexDirection:'column',alignItems:'center', top:'20%'}}><h2>You have to be logged in to use this function</h2><h3>Click to close</h3></div>:null}
     </div>
